9f1170418b42074c8a673a272d8c055678036d05
Jean Bikorimana <94160288+Jeanuisb@users.noreply.github.com>
2024-01-14 20:54:37 -0600

    Developed log in page. add logos. "admin" for username and password not runnable. issue on MainActivity.kt

--- app/src/main/java/com/example/cuisinesync/MainActivity.kt
+++ app/src/main/java/com/example/cuisinesync/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.cuisinesync
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,35 +17,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cuisinesync.ui.theme.CuisineSyncTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            CuisineSyncTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText username =(EditText) findViewById (R.id.username);
+        EditText password =(TextView) findViewById(R.id.password);
+
+        Button loginbtn =(Button) findViewById(R.id.loginbtn);
+
+        //set username and password to admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //valid user
+                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                }
+                else{ //invalid user
+                    Toast.makeText(MainActivity.this,"Invalid Username or Password",Toast.LENGTH_SHORT).show();
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CuisineSyncTheme {
-        Greeting("Android")
+        });
     }
 }

1b8249e2cf8f373b33f37f52a1c82d2dfe338b8b
Jean Bikorimana <94160288+Jeanuisb@users.noreply.github.com>
2024-01-14 22:17:20 -0600

    Hard Stop. App not opening in the emulator

--- app/src/main/java/com/example/cuisinesync/MainActivity.kt
+++ app/src/main/java/com/example/cuisinesync/MainActivity.kt
@@ -3,43 +3,27 @@ package com.example.cuisinesync
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cuisinesync.ui.theme.CuisineSyncTheme
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        EditText username =(EditText) findViewById (R.id.username);
-        EditText password =(TextView) findViewById(R.id.password);
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val loginbtn = findViewById<Button>(R.id.loginbtn)
 
-        Button loginbtn =(Button) findViewById(R.id.loginbtn);
-
-        //set username and password to admin
-
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    //valid user
-                    Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
-                }
-                else{ //invalid user
-                    Toast.makeText(MainActivity.this,"Invalid Username or Password",Toast.LENGTH_SHORT).show();
-                }
+        loginbtn.setOnClickListener {
+            if (username.text.toString() == "admin" && password.text.toString() == "admin") {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Invalid Username or Password", Toast.LENGTH_SHORT).show()
             }
-        });
+        }
     }
 }

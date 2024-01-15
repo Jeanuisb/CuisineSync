2378725d63dd528abea6f88a9fdcce8f2b05c83d
Jean Bikorimana <94160288+Jeanuisb@users.noreply.github.com>
2024-01-15 00:48:24 -0600

    Got login page working. Cooked!

--- app/src/main/java/com/example/cuisinesync/MainActivity.kt
+++ app/src/main/java/com/example/cuisinesync/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.cuisinesync
-
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
 
 
